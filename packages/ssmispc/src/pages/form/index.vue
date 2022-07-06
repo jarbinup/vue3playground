153e8302208536b8f451c6d80f80e589da54ae51
YANG <jarbinup@qq.com>
2022-07-06 12:18:56 +0000

    feat: add table form

--- packages/ssmispc/src/pages/form/index.vue
+++ packages/ssmispc/src/pages/form/index.vue
@@ -6,74 +6,67 @@
     label-width="120px"
     class="demo-ruleForm"
     :size="formSize"
-    status-icon
   >
-    <el-form-item label="Activity name" prop="name">
+    <el-form-item label="活动名称" prop="name">
       <el-input v-model="ruleForm.name" />
     </el-form-item>
-    <el-form-item label="Activity zone" prop="region">
-      <el-select v-model="ruleForm.region" placeholder="Activity zone">
-        <el-option label="Zone one" value="shanghai" />
-        <el-option label="Zone two" value="beijing" />
-      </el-select>
+    <el-form-item label="活动地区" prop="region">
+      <el-select-v2 v-model="ruleForm.region" placeholder="活动地区" :options="REGION_LIST" />
     </el-form-item>
-    <el-form-item label="Activity count" prop="count">
-      <el-select-v2
-        v-model="ruleForm.count"
-        placeholder="Activity count"
-        :options="options"
-      />
+    <el-form-item label="活动次数" prop="count">
+      <el-select-v2 v-model="ruleForm.count" placeholder="活动次数" :options="options" />
     </el-form-item>
-    <el-form-item label="Activity time" required>
-      <el-col :span="11">
-        <el-form-item prop="date1">
-          <el-date-picker
-            v-model="ruleForm.date1"
-            type="date"
-            label="Pick a date"
-            placeholder="Pick a date"
-            style="width: 100%"
-          />
-        </el-form-item>
-      </el-col>
-      <el-col class="text-center" :span="2">
-        <span class="text-gray-500">-</span>
-      </el-col>
-      <el-col :span="11">
-        <el-form-item prop="date2">
-          <el-time-picker
-            v-model="ruleForm.date2"
-            label="Pick a time"
-            placeholder="Pick a time"
-            style="width: 100%"
-          />
-        </el-form-item>
-      </el-col>
-    </el-form-item>
-    <el-form-item label="Instant delivery" prop="delivery">
+    <el-form-item label="立即配送" prop="delivery">
       <el-switch v-model="ruleForm.delivery" />
     </el-form-item>
-    <el-form-item label="Activity type" prop="type">
+    <el-form-item label="活动类型" prop="type">
       <el-checkbox-group v-model="ruleForm.type">
-        <el-checkbox label="Online activities" name="type" />
-        <el-checkbox label="Promotion activities" name="type" />
-        <el-checkbox label="Offline activities" name="type" />
-        <el-checkbox label="Simple brand exposure" name="type" />
+        <el-checkbox v-for="item in ACTIVITY_TYPE_LIST" :label="item.label" :value="item.value" name="type"/>
       </el-checkbox-group>
     </el-form-item>
-    <el-form-item label="Resources" prop="resource">
+    <el-form-item label="活动来源" prop="resource">
       <el-radio-group v-model="ruleForm.resource">
         <el-radio label="Sponsorship" />
         <el-radio label="Venue" />
       </el-radio-group>
     </el-form-item>
-    <el-form-item label="Activity form" prop="desc">
+    <el-form-item label="活动描述" prop="desc">
       <el-input v-model="ruleForm.desc" type="textarea" />
     </el-form-item>
+    <el-form-item label="活动配置">
+      <div class="table-wrap">
+        <el-button @click="handleAdd">添加配置</el-button>
+        <el-table :data="ruleForm.config">
+          <el-table-column label="配置名称">
+            <template #default="scope" class="table-column-wrap">
+            <div style="padding: 20px 0">
+              <el-form-item :prop="`config.${scope.$index}.itemName`" :rules="rules.itemName">
+                <el-input v-model="scope.row.itemName" />
+              </el-form-item>
+            </div>
+            </template>
+          </el-table-column>
+          <el-table-column label="配置值">
+            <template #default="scope">
+            <div style="padding: 20px 0">
+              <el-form-item :prop="`config.${scope.$index}.itemValue`" :rules="rules.itemValue">
+                <el-input v-model="scope.row.itemValue" />
+              </el-form-item>
+            </div>
+            </template>
+          </el-table-column>
+          <el-table-column label="操作">
+            <template #default="scope">
+              <el-button type="text" @click="handleDelete(scope.$index)">删除</el-button>
+              <el-button type="text" @click="handleDelete(scope.$index)">上移</el-button>
+              <el-button type="text" @click="handleDelete(scope.$index)">下移</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+      </div>
+    </el-form-item>
     <el-form-item>
-      <el-button type="primary" @click="submitForm(ruleFormRef)"
-        >Create</el-button
-      >
+      <el-button type="primary" @click="submitForm(ruleFormRef)">Create</el-button>
       <el-button @click="resetForm(ruleFormRef)">Reset</el-button>
     </el-form-item>
   </el-form>
@@ -82,7 +75,7 @@
 <script lang="ts" setup>
 import { reactive, ref } from 'vue'
 import type { FormInstance, FormRules } from 'element-plus'
-import { ruleConfig } from './utils/const'
+import { RULES, ACTIVITY_TYPE_LIST, REGION_LIST } from './utils/const'
 
 const formSize = ref('default')
 const ruleFormRef = ref<FormInstance>()
@@ -96,13 +89,19 @@ const ruleForm = reactive({
   type: [],
   resource: '',
   desc: '',
+  config: [{
+    itemName: '',
+    itemValue: ''
+  }]
 })
 
-const rules = reactive<FormRules>(ruleConfig)
+const rules = reactive<FormRules>(RULES)
 
 const submitForm = async (formEl: FormInstance | undefined) => {
   if (!formEl) return
   await formEl.validate((valid, fields) => {
+    console.log('valid', valid)
+    console.log('fields', fields)
     if (valid) {
       console.log('submit!')
     } else {
@@ -120,4 +119,23 @@ const options = Array.from({ length: 100 }).map((_, idx) => ({
   value: `${idx + 1}`,
   label: `${idx + 1}`,
 }))
+
+const handleAdd = () => {
+  ruleForm.config.push({
+    itemName: '',
+    itemValue: ''
+  })
+}
+
+const handleDelete = (index: number) => {
+  ruleForm.config.splice(index, 1)
+}
+
 </script>
+<style >
+.table-wrap {
+  width: 100%;
+  text-align: right;
+}
+
+</style>
--- packages/ssmispc/src/pages/form/utils/const.ts
+++ packages/ssmispc/src/pages/form/utils/const.ts
@@ -1,56 +1,15 @@
-export const ruleConfig = {
+export const RULES = {
   name: [
-    { required: true, message: 'Please input Activity name', trigger: 'blur' },
-    { min: 3, max: 5, message: 'Length should be 3 to 5', trigger: 'blur' },
+    { required: true, message: '请输入活动名', trigger: 'blur' },
+    { min: 3, max: 5, message: '长度为 3-5 个字符', trigger: 'blur' }
   ],
-  region: [
-    {
-      required: true,
-      message: 'Please select Activity zone',
-      trigger: 'change',
-    },
-  ],
-  count: [
-    {
-      required: true,
-      message: 'Please select Activity count',
-      trigger: 'change',
-    },
-  ],
-  date1: [
-    {
-      type: 'date',
-      required: true,
-      message: 'Please pick a date',
-      trigger: 'change',
-    },
-  ],
-  date2: [
-    {
-      type: 'date',
-      required: true,
-      message: 'Please pick a time',
-      trigger: 'change',
-    },
-  ],
-  type: [
-    {
-      type: 'array',
-      required: true,
-      message: 'Please select at least one activity type',
-      trigger: 'change',
-    },
-  ],
-  resource: [
-    {
-      required: true,
-      message: 'Please select activity resource',
-      trigger: 'change',
-    },
-  ],
-  desc: [
-    { required: true, message: 'Please input activity form', trigger: 'blur' },
-  ],
+  region: [{ required: true, message: '请选择活动地区', trigger: 'change' }],
+  count: [ { required: true, message: '请选择活动次数', trigger: 'change' }],
+  type: [{ type: 'array', required: true, message: '请至少选择一种活动类型', trigger: 'change'},],
+  resource: [{ required: true, message: '请选择活动来源', trigger: 'change'}],
+  desc: [{ required: true, message: '请输入活动描述', trigger: 'blur' }],
+  itemName: [{ required: true, message: '请输入配置名称', trigger: 'blur' }],
+  itemValue: [{ required: true, message: '请输入配置值', trigger: 'blur' }]
 }
 
 export const ACTIVITY_TYPE = {
@@ -59,3 +18,35 @@ export const ACTIVITY_TYPE = {
   OFFLINE: 3,
   SIMPLE: 4,
 }
+
+export const ACTIVITY_TYPE_LIST = [
+  {
+    value: ACTIVITY_TYPE.ONLINE,
+    label: '线上活动',
+    key: ACTIVITY_TYPE.ONLINE,
+  },
+  {
+    value: ACTIVITY_TYPE.PROMOTION,
+    label: '促销活动',
+    key: ACTIVITY_TYPE.PROMOTION,
+  },
+  {
+    value: ACTIVITY_TYPE.OFFLINE,
+    label: '线下活动',
+    key: ACTIVITY_TYPE.OFFLINE,
+  },
+  {
+    value: ACTIVITY_TYPE.SIMPLE,
+    label: '简单活动',
+    key: ACTIVITY_TYPE.SIMPLE,
+  },
+];
+
+export const REGION_LIST = [
+  { value: 'beijing', label: '北京', key: 'beijing' },
+  { value: 'shanghai', label: '上海', key: 'shanghai' },
+  { value: 'guangzhou', label: '广州', key: 'guangzhou' },
+  { value: 'shenzhen', label: '深圳', key: 'shenzhen' }
+];
+
+
